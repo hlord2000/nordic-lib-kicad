e0f2350e9357c7604f47e75716beab508d9a0877
hlord2000 <helmut@helmutlord.com>
2023-07-22 02:09:24 -0400

    Fixed download URL

--- PCM/create_pcm_archive.sh
+++ PCM/create_pcm_archive.sh
@@ -41,5 +41,5 @@ echo "Gather data for repo rebuild"
 echo VERSION=$VERSION >> $GITHUB_ENV
 echo DOWNLOAD_SHA256=$(shasum --algorithm 256 PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f1) >> $GITHUB_ENV
 echo DOWNLOAD_SIZE=$(ls -l PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f5) >> $GITHUB_ENV
-echo DOWNLOAD_URL="https:\/\/github.com\/ebastler\/marbastlib\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip" >> $GITHUB_ENV
+echo DOWNLOAD_URL="https:\/\/github.com\/hlord2000\/nordic-lib-kicad\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip" >> $GITHUB_ENV
 echo INSTALL_SIZE=$(unzip -l PCM/KiCAD-PCM-$VERSION.zip | tail -1 | xargs | cut -d' ' -f1) >> $GITHUB_ENV
